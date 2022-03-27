1088a989ff9ed208f90e603f7269c1c0684dc6f6
Matthew Yeung <matthewyeung34@gmail.com>
2022-03-27 16:30:32 -0400

    Initial commit

---
+++ app/src/main/java/com/matthewyeung35/helloworld/MainActivity.java
@@ -0,0 +1,30 @@
+package com.matthewyeung35.helloworld;
+
+import android.os.Bundle;
+
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.view.View;
+
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
+
+import com.matthewyeung35.helloworld.databinding.ActivityMainBinding;
+
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.content_main);
+
+        TextView txtHello = findViewById()
+    }
+}
